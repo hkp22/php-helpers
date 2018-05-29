d9cfc4213811748dc64cc94fd4462d97a4071f00
Harish Kumar <harish.pra22@gmail.com>
2018-05-29 23:29:33 +0530

    dd() and value() helper functions added.

---
+++ src/miscellaneous.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
+    }
+}
+
+if (!function_exists('dd')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function dd()
+    {
+        array_map(function ($x) {
+            var_dump($x);
+        }, func_get_args());
+
+        die(1);
+    }
+}
