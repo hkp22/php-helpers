1756292fc220af9f3d030ff310f4ac0d67984ada
Harish Kumar <harish.pra22@gmail.com>
2018-05-29 23:31:57 +0530

    array_add(), array_get() and array_set() helper function added

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
-# php-helpers
-Php Helpers inspired by Laravel Helpers for non-laravel projects.
+## PHP Helper Functions extracted from Laravel
+
+This project has extracted useful [helper functions from laravel framework](http://laravel.com/docs/5.6/helpers), which can be used outside Laravel.
+
+## Installation
+
+You can install the package via composer:
+
+```bash
+composer require hkp22/php-helpers
+```
+
+## Usage
+
+* **[Arrays](#arrays)**
+  * [array_add()](#array_add)
+  * [array_get()](#array_get)
+  * [array_set()](#array_set)
+* **[Miscellaneous](#miscellaneous)**
+  * [dd()](#dd)
+  * [value()](#value)
+
+
+## Arrays
+<a name="arrays"></a>
+
+
+### `array_add()`
+<a name="array_add"></a>
+
+The `array_add` function adds a given key/value pair to an array if the given key doesn't already exist in the array:
+
+```php
+$array = array_add(['name' => 'Desk'], 'price', 100);
+
+// ['name' => 'Desk', 'price' => 100]
+```
+
+### `array_get()`
+<a name="array_get"></a>
+
+The `array_get` function retrieves a value from a deeply nested array using "dot" notation:
+
+```php
+$array = ['products' => ['desk' => ['price' => 100]]];
+
+$price = array_get($array, 'products.desk.price');
+
+// 100
+```
+
+### `array_set()`
+<a name="array_set"></a>
+
+The `array_set` function sets a value within a deeply nested array using "dot" notation:
+
+```php
+$array = ['products' => ['desk' => ['price' => 100]]];
+
+array_set($array, 'products.desk.price', 200);
+
+// ['products' => ['desk' => ['price' => 200]]]
+```
+
+## Miscellaneous
+<a name="miscellaneous"></a>
+
+
+### `dd()`
+<a name="dd"></a>
+
+The `dd` function dumps the given variables and ends execution of the script:
+
+```php
+dd($value);
+
+dd($value1, $value2, $value3, ...);
+```
+
+### `value()`
+<a name="value"></a>
+
+The `value` function returns the value it is given. However, if you pass a Closure to the function, the Closure will be executed then its result will be returned:
+
+```php
+$result = value(true);
+
+// true
+
+$result = value(function () {
+    return false;
+});
+
+// false
+```
---
+++ src/array.php
@@ -0,0 +1,86 @@
+<?php
+
+if (!function_exists('array_set')) {
+    /**
+     * Set an array item to a given value using "dot" notation.
+     *
+     * If no key is given to the method, the entire array will be replaced.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return array
+     */
+    function array_set(&$array, $key, $value)
+    {
+        if (is_null($key)) {
+            return $array = $value;
+        }
+
+        $keys = explode('.', $key);
+
+        while (count($keys) > 1) {
+            $key = array_shift($keys);
+
+            // If the key doesn't exist at this depth, we will just create an empty array
+            // to hold the next value, allowing us to create the arrays to hold final
+            // values at the correct depth. Then we'll keep digging into the array.
+            if (!isset($array[$key]) || !is_array($array[$key])) {
+                $array[$key] = [];
+            }
+            $array = &$array[$key];
+        }
+
+        $array[array_shift($keys)] = $value;
+
+        return $array;
+    }
+}
+
+if (!function_exists('array_get')) {
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function array_get($array, $key, $default = null)
+    {
+        if (is_null($key)) {
+            return $array;
+        }
+
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (!is_array($array) || !array_key_exists($segment, $array)) {
+                return value($default);
+            }
+            $array = $array[$segment];
+        }
+
+        return $array;
+    }
+}
+
+if (!function_exists('array_add')) {
+    /**
+     * Add an element to an array using "dot" notation if it doesn't exist.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return array
+     */
+    function array_add($array, $key, $value)
+    {
+        if (is_null(array_get($array, $key))) {
+            array_set($array, $key, $value);
+        }
+        return $array;
+    }
+}
